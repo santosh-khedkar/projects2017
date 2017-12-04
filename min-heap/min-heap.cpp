23e08f5077668b352df1c52bd6e82bd122fd3b23
Santosh Narayankhedkar <santoshn@usc.edu>
2017-12-04 12:46:57 -0800

    MinHeap implementation

--- min-heap/min-heap.cpp
+++ min-heap/min-heap.cpp
@@ -4,81 +4,76 @@
 using namespace std;
 
 class Minheap {
-	int heap_size;
-	int cap;
-	int *harr;
+	int *harr;  /*array emulating the minheap*/
+	int cap;  /*total capacity of the minheap*/
+	int heap_size; /*current heap size*/
 
 public:
-	Minheap(int cap);
-
-	int parent (int i) { 
-		return i - 1;
+	Minheap(int capacity);
+	int parent (int i) {
+		return (i - 1) / 2;
 	}
-
-	int left (int i) {
-		return 2 * i + 1;
+	int left (int i){
+		return (2 * i + 1);
 	}
-
-	int right (int i) {
-		return 2 * i + 2;
+	int right (int i){
+		return (2 * i + 2);
 	}
-
-	void decrease_key (int i, int value);
-	void Minheapify(int i);
-	void Insert_key(int value);
-	void delete_key(int i);
-
-	int get_min(){
+	int getMin(){
 		return harr[0];
 	}
-	int extract_min();
+	void insertKey(int key);
+	void deleteKey(int i);
+	void decreaseKey (int i, int value);
+	int extractMin();
+	void MinHeapify(int i);
+	void printDS();
 };
 
-void swap (int *x , int *y){
+void swap (int *x, int *y){
 	int temp = *x;
 	*x = *y;
 	*y = temp;
 }
 
-Minheap::Minheap (int capacity){
-	heap_size = 0;
+Minheap::Minheap(int capacity){
 	cap = capacity;
+	heap_size = 0;
 	harr = new int [cap];
 }
 
-void Minheap::decrease_key (int i, int value) {
-	harr[i] = value;
-
-	while (i!=0 && harr[i] < harr[parent(i)]){
-		swap(&harr[i], &harr[parent(i)]);
-		i = parent(i);
-	}
-}
-
-
-void Minheap::Insert_key(int value) {
-	
-	if(heap_size == cap){
-		cout << "Heap at capacity";
+void Minheap::insertKey(int key){
+	if (heap_size >= cap){
+		cout <<"Heap at capacity"<<endl;
 		return;
 	}
-
+	heap_size++;
 	int i = heap_size - 1;
-	harr[i] = value;
+	harr[i] = key;
 
-	while (i !=0 && harr[i] < harr[parent(i)]){
+	while (i != 0 && harr[i] < harr[parent(i)]){
 		swap(&harr[i], &harr[parent(i)]);
 		i = parent(i);
 	}
 }
 
-void Minheap::delete_key(int i) {
-	decrease_key(i, INT_MIN);
-	extract_min();
+void Minheap::decreaseKey(int i, int value){
+	harr[i] = value;
+
+	while (i != 0 && harr[i] < harr[parent(i)]){
+		swap(&harr[i], &harr[parent(i)]);
+		i = parent(i);
+	}
 }
 
-int Minheap::extract_min() {
+void Minheap::deleteKey(int i){
+	decreaseKey(i, INT_MIN);
+	extractMin();
+}
+
+int Minheap::extractMin(){
 	if (heap_size == 0){
+		cout <<"Heap is empty"<<endl;
 		return INT_MAX;
 	}
 	if (heap_size == 1){
@@ -87,42 +82,49 @@ int Minheap::extract_min() {
 	}
 
 	int root = harr[0];
-	harr[0] = harr[heap_size - 1];
+	harr[0] = harr[heap_size-1];
 	heap_size--;
-	Minheapify(0);
+	MinHeapify(0);
 	return root;
 }
 
-void Minheap::Minheapify(int i) {
+void Minheap::MinHeapify(int i){
+	int smallest = i;
 	int l = left(i);
 	int r = right(i);
-	int smallest = i;
 
 	if (l < heap_size && harr[l] < harr[smallest]){
 		smallest = l;
 	}
 	if (r < heap_size && harr[r] < harr[smallest]){
-		smallest = r;
+		smallest = i;
 	}
 	if (smallest != i){
 		swap(&harr[i], &harr[smallest]);
-		Minheapify(smallest);
+		MinHeapify(smallest);
 	}
-
 }
-int main(){
-	Minheap min_heap(11);
-	min_heap.Insert_key(3);
-	min_heap.Insert_key(2);
-	min_heap.delete_key(1);
-	min_heap.Insert_key(15);
-	min_heap.Insert_key(5);
-	min_heap.Insert_key(4);
-	min_heap.Insert_key(45);
-	cout <<min_heap.extract_min() <<endl;
-	cout <<min_heap.get_min()<<endl;
-	min_heap.decrease_key(2,1);
-	cout<<min_heap.get_min();
 
+void Minheap::printDS(){
+	for (int i = 0 ; i< heap_size; i++)
+		cout <<harr[i] <<" ";
+	cout<<endl;
+}
+
+int main(){
+	Minheap mh(11);
+	mh.insertKey(3);
+	mh.insertKey(2);
+	mh.deleteKey(1);
+	mh.insertKey(15);
+	mh.insertKey(5);
+	mh.insertKey(4);
+	mh.insertKey(45);
+	mh.printDS();
+	cout <<mh.extractMin() <<endl;
+	cout <<mh.getMin()<<endl;
+	mh.decreaseKey(2,1);
+	cout<<mh.getMin()<<endl;
+	mh.printDS();
 	return 0;
 }
